0cf6a4144eb919036b118e9b98cc7b34ca7d830d
Katie Bridson <kbridson@memphis.edu>
2019-11-04 16:31:11 -0600

    Added some useful yarn packages, installed and set up bootstrap and bootswatch with webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,43 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  "window.jQuery": 'jquery',
+  "window.$": 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
+environment.config.merge({
+  module: {
+    rules: [
+      {
+        test: require.resolve('jquery'),
+        use: [{
+          loader: 'expose-loader',
+          options: '$'
+        }, {
+          loader: 'expose-loader',
+          options: 'jQuery'
+        }]
+      },
+      {
+        test: require.resolve('@rails/ujs'),
+        use: [{
+          loader: 'expose-loader',
+          options: 'Rails'
+        }]
+      },
+      {
+        test: require.resolve('autosize'),
+        use: [{
+          loader: 'expose-loader',
+          options: 'autosize'
+        }]
+      }
+    ]
+  }
+})
 
 module.exports = environment
