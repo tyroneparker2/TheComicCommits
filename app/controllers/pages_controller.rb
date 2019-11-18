802c06e559bdd1d4ea6640dbedf886126c90fe59
Brandon Smith <BrandonTheSmith97@gmail.com>
2019-11-18 16:29:29 -0600

    Homepage displays

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :authorized, only: [:homepage]
   def homepage
     respond_to do |format|
       format.html { render :homepage }
